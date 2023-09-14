cd194b8ac6f54c10621efeefd9283ccddace839a
ThiagoP08 <thiago.basso72@gmail.com>
2023-09-14 14:09:29 -0300

    Up

---
+++ java-back-tarefas/.metadata/.plugins/org.eclipse.core.resources/.history/a/c0b8c52e8252001e182c973d0639363b
@@ -0,0 +1,27 @@
+package model;
+
+public class Tarefa {
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Boolean getStatus() {
+		return status;
+	}
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+	
+	private Integer id;
+	private String descricao;
+	private Boolean status;
+}
--- java-back-tarefas/backtodo/src/main/java/App.java
+++ java-back-tarefas/backtodo/src/main/java/App.java
@@ -17,33 +17,58 @@ public class App {
 
         lista.forEach(tarefa -> System.out.println(tarefa.getDescricao()));
 
-        Tarefa novaTarefa = new Tarefa(); 
-        novaTarefa.setDescricao("Estudar para a prova");
+        int consultarTarefar = 1;
+
+        Tarefa tarefaConsulta = dao.consultarPorId(consultarTarefar);
+
+        if (tarefaConsulta != null) {
+            System.out.println("Tarefa encontrada:");
+            System.out.println("ID: " + tarefaConsulta.getId());
+            System.out.println("Descrição: " + tarefaConsulta.getDescricao());
+            System.out.println("Status: " + tarefaConsulta.getStatus());
+        } else {
+            System.out.println("Tarefa não encontrada para: " + consultarTarefar);
+        }
+
+        /*Tarefa novaTarefa = new Tarefa(); 
+        novaTarefa.setDescricao("Nova Tarefa");
         novaTarefa.setStatus(false);
 
         if (dao.incluir(novaTarefa)) { 
             System.out.println("Tarefa incluída com sucesso");
         } else {
             System.out.println("Tarefa não foi incluída");
-        }
+        }*/
 
-        Tarefa tarefaEditar = new Tarefa(); 
-        tarefaEditar.setId(1);
-        tarefaEditar.setDescricao("Nova descrição");
-        tarefaEditar.setStatus(true);
+        /*Tarefa editarTarefa= new Tarefa(); 
+        editarTarefa.setId(1);
+        editarTarefa.setDescricao("Estudar para a prova");
+        editarTarefa.setStatus(false);
 
-        if (dao.editar(tarefaEditar)) { 
+        if (dao.editar(editarTarefa)) { 
             System.out.println("Tarefa editada com sucesso");
         } else {
             System.out.println("Não foi possível editar a tarefa");
-        }
+        } */
 
-        int idTarefaExcluir = 3;
+        /*int excluirTarefa = 3;
 
-        if (dao.excluir(idTarefaExcluir)) {
+        if (dao.excluir(excluirTarefa)) {
             System.out.println("Tarefa excluída com sucesso");
         } else {
             System.out.println("Não foi possível excluir a tarefa");
-        }
+            System.out.println();
+        } */
     }
 }
+
+
+
+
+
+
+
+
+
+
+
